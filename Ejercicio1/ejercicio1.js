d632cb8410c494f26328688284b9241cdf1ccab3
DiegoBlazquezRosado <diegoblazquezr@gmail.com>
2024-05-13 16:45:24 +0200

    Only js

---
+++ Ejercicio1/ejercicio1.js
@@ -0,0 +1,11 @@
+let frase = "Esto es un ejemplo de frase gracias.";
+let letra = "E";
+let contador = 0;
+
+for (let i = 0; i < frase.length; i++) {
+    if (frase[i].toLowerCase() === letra.toLowerCase()) {
+        contador++;
+    }
+};
+
+console.log("Se repite " + contador + " veces.");
---
+++ Ejercicio2/ejercicio2.js
@@ -0,0 +1,7 @@
+const num = 49;
+
+for (let i = num; i < num + 50 ; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
