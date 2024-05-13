43321997c840b7b067e489fc706d528e02264e60
DiegoBlazquezRosado <diegoblazquezr@gmail.com>
2024-05-13 16:58:46 +0200

    same

--- Ejercicio1/ejercicio1.js
+++ Ejercicio1/ejercicio1.js
@@ -2,6 +2,8 @@ let frase = "Esto es un ejemplo de frase gracias.";
 let letra = "E";
 let contador = 0;
 
+console.log(frase);
+
 for (let i = 0; i < frase.length; i++) {
     if (frase[i].toLowerCase() === letra.toLowerCase()) {
         contador++;
