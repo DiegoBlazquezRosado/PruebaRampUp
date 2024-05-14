d1eab16968b2fdfb0a1607c66e4b05c1f585d6b8
DiegoBlazquezRosado <diegoblazquezr@gmail.com>
2024-05-14 10:38:00 +0200

    Updated Syntax

--- Ejercicio1/ejercicio1.js
+++ Ejercicio1/ejercicio1.js
@@ -1,14 +1,11 @@
-function  cuentaLetras() {
-    let frase = "Esto es un ejemplo de frase gracias.";
-    let letra = "e";
+function  contarLetras(frase, letra) {
     let contador = 0;
-
     for (let i=0; i<frase.length; i++) {
         if (frase[i].toLowerCase() === letra.toLowerCase()) {
             contador++;
         }
     }
-    return console.log("La letra " + letra.toUpperCase() + " se repite " + contador + " veces.");
+    return console.log("En la frase (" + frase + ") la letra (" + letra.toUpperCase() + ") se repite (" + contador + ") veces.");
 }
 
-cuentaLetras();
+contarLetras("Esto es un ejemplo de frase gracias.", "e");
--- Ejercicio2/ejercicio2.js
+++ Ejercicio2/ejercicio2.js
@@ -1,12 +1,9 @@
-function sacarImpares() {
-    let num = 0;
-
-    for (let i=num ; i<num+50 ; i++) {
+function sacarImpares(num) {
+    for (let i=num; i<(num+50); i++) {
         if (i % 2 !== 0) {
             console.log(i);
         }
     }
-    return;
 }
 
-sacarImpares();
+sacarImpares(0);
